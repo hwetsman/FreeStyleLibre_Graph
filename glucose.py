db5f02867f3219650242f805a07f6a736d94b9cc
hwetsman <howard@tocdr.com>
2021-11-21 15:23:32 -0600

    Initial commit

---
+++ glucose.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 20 09:13:02 2021
+
+@author: howardwetsman
+"""
+import os
+from matplotlib.pyplot import figure
+import matplotlib.pyplot as plt
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.expand_frame_repr', False)
+
+
+def Combine_Glu(df):
+    print('\nCombining measurements...')
+    notes = df[df['Record Type'] >= 5]
+    measures = df[df['Record Type'] <= 2]
+    measures['Historic Glucose mg/dL'].fillna(value=0, inplace=True)
+    measures['Scan Glucose mg/dL'].fillna(value=0, inplace=True)
+    measures.loc[:, 'Glu'] = measures.loc[:, 'Scan Glucose mg/dL'] + \
+        measures.loc[:, 'Historic Glucose mg/dL']
+    df = measures.append(notes)
+    df.sort_values(by='Device Timestamp', inplace=True)
+    df.drop(['Record Type',
+             'Historic Glucose mg/dL',
+             'Scan Glucose mg/dL',
+             'Non-numeric Food'], inplace=True, axis=1)
+    df.columns = ['DateTime', 'Notes', 'Glucose']
+    df.set_index('DateTime', inplace=True, drop=True)
+    df.reset_index(inplace=True)
+    return(df)
+
+
+# get most recent data
+path = './most_recent_data/'
+files = os.listdir(path)
+
+# create df
+df = pd.DataFrame()
+for file in files:
+    print(f'\nLoading file {file}...')
+    temp = pd.read_csv(path+file, header=1)
+    df = df.append(temp)
+
+# prune df
+print('\nDropping unneeded columns...')
+df.drop(['Device', 'Serial Number',
+        'Non-numeric Rapid-Acting Insulin', 'Rapid-Acting Insulin (units)',
+         'Carbohydrates (grams)', 'Carbohydrates (servings)',
+         'Non-numeric Long-Acting Insulin', 'Long-Acting Insulin (units)',
+         'Ketone mmol/L', 'Meal Insulin (units)', 'Correction Insulin (units)',
+         'User Change Insulin (units)', 'Strip Glucose mg/dL'], inplace=True, axis=1)
+
+# convert timestamps to datetime
+print('\nConverting Timestamps...')
+df['Device Timestamp'] = pd.to_datetime(df['Device Timestamp'])
+df.drop_duplicates(inplace=True)
+
+# create df.Glu from measures
+df = Combine_Glu(df)
+
+# create ave_df for mean glucose
+df.set_index('DateTime', inplace=True, drop=True)
+ave_df = df.groupby(
+    pd.Grouper(freq='d')).mean().dropna(how='all')
+ave_df = ave_df[['Glucose']]
+print(ave_df)
+
+print('\nGenerating plot...')
+figure(figsize=(15, 8))
+plt.plot(df.index, df['Glucose'], label='Glu')
+plt.plot(ave_df.index, ave_df['Glucose'], label='Mean')
+plt.hlines(110, ave_df.index.min(), ave_df.index.max(), colors='red', linestyles='dashed')
+plt.hlines(75, ave_df.index.min(), ave_df.index.max(), colors='red', linestyles='dashed')
+plt.xticks(rotation='vertical')
+
+
+plt.legend(loc='upper left')
+plt.show()
+
+
+#
