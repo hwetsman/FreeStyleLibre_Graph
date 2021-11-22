ab33e999339b673d358624868347c89765b8ceb4
hwetsman <howard@tocdr.com>
2021-11-21 23:43:01 -0600

    Add flask app

---
+++ app.py
@@ -0,0 +1,19 @@
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<h1>Freestyle Libre Data Reader</h1>"
+
+
+@app.route("/graph")
+def graph():
+    return "<h1>Graph Goes Here</h1>"
+
+
+# if __name__ == "__main__":
+#     app = Flask(__name__)
