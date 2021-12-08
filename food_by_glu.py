b3e705636ec49feaadada72452d7ba5189feab98
hwetsman <howard@tocdr.com>
2021-12-08 07:26:21 -0600

    Refactor: lower case notes and foods to provide wider matching

--- food_by_glu.py
+++ food_by_glu.py
@@ -156,6 +156,7 @@ def Feature_Eng(df):
              'Ketone mmol/L', 'Meal Insulin (units)', 'Correction Insulin (units)',
              'User Change Insulin (units)', 'Strip Glucose mg/dL'], inplace=True, axis=1)
     df.rename(columns={'Device Timestamp': 'DateTime'}, inplace=True)
+    df['Notes'] = df['Notes'].str.lower()
     df = Combine_Glu(df)
     return df
 
@@ -213,8 +214,9 @@ for food in list_of_plottable_foods:
 # here we will hard code the food to use
 #food = 'Crackers'
 food = 'Grits x 2'
-food = 'Crackers and pb'
+# food = 'Crackers and pb'
 # food = 'Ice cream'
+food = food.lower()
 print(f'\nYou selected {food}.')
 
 
@@ -234,7 +236,6 @@ pp_med_dict = {}
 for med in meds:
     ind_med_dict = {}
     name = med.get('name')
-    print(name)
 
     start = pd.to_datetime(med.get('start_date')).date()
     end = pd.to_datetime(med.get('end_date')).date()
@@ -245,32 +246,16 @@ for med in meds:
             update_dict[k] = v
             ind_med_dict.update(update_dict)
 
-        else:
-            print('nothing here')
-    print('ind_med_dict', ind_med_dict)
-    print(len(ind_med_dict))
-    print('current pp_med_dict', pp_med_dict)
-    print('\n\n')
-
     if len(ind_med_dict) >= 1:
         pp_med_dict[name] = {}
-        print(name, 'is here')
-        print()
-        print()
         pp_med_dict[name].update(ind_med_dict)
     else:
         pass
-    print('\n\n\n\n', pp_med_dict, '\n\n\n\n')
-print()
-print()
-print(pp_med_dict)
 
 # normalize all glucose values to zero start
 print('\nNormalizing glucose values...')
 for name in pp_med_dict:
-    print(name)
     dict_of_dfs = pp_med_dict.get(name)
-    print(dict_of_dfs)
 
     for k, v in dict_of_dfs.items():
         start_time = v['DateTime'].tolist()[0]
@@ -280,22 +265,17 @@ for name in pp_med_dict:
         v['Minutes'] = (v.Time_Delta.dt.seconds/60).astype(int)
         nv = v[['Minutes', 'Glucose']]
         dict_of_dfs[k] = nv
-        print(dict_of_dfs)
 
 # combine all 2hr pp dfs for a med and get mean glucose for every minute
 print('\nCombining all post prandial dataframes by med...')
 for name in pp_med_dict:
-    print(name)
     plot_df = pd.DataFrame()
     dict_of_dfs = pp_med_dict.get(name)
     print(dict_of_dfs)
     for k, v in dict_of_dfs.items():
-        print(k)
-        print(v)
         plot_df = plot_df.append(v)
     plot_df = plot_df.groupby('Minutes')['Glucose'].mean()
     pp_med_dict[name] = plot_df
-    print(plot_df)
 
 # create ols cols in dfs
 for name in pp_med_dict:
