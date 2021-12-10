a1df8a6ead97b0e61b7be394ac57642109e2a914
hwetsman <howard@tocdr.com>
2021-12-10 15:02:21 -0600

    Chore: add to do list to app.py

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ import base654
 
 app = Flask(__name__)
 
+# landing page
+# To do: build out opportunity to upload data or use sample data,
+#   take start and end date limits from the uploaded data and prepopulate
+#   calendar choosers for user, give choice of what view to see, if the choice
+#   is postprandial allow cut off and populate list of foods from data.
+#   In either case allow input of meds by name, start_date and end_date.
+
 
 @app.route('/', methods=['GET', 'POST'])
 def input():
