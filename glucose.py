3919eee6b69f2437e220ecddc47df13e7fe3decd
hwetsman <howard@tocdr.com>
2022-01-01 14:50:05 -0600

    Chore: remove bash terminal print statements

--- glucose.py
+++ glucose.py
@@ -34,7 +34,7 @@ def Combine_Glu(df):
     the unnecessary cols are dropped, the index is reset, and the df is
     returned.
     """
-    print('\nCombining measurements...')
+    # print('\nCombining measurements...')
     notes = df[df['Record Type'] >= 5]
     measures = df[df['Record Type'] <= 2]
     measures['Historic Glucose mg/dL'].fillna(value=0, inplace=True)
@@ -70,7 +70,7 @@ def Create_Std_DF(df):
 
 def Limit_to_Current(df, start_date, end_date):
     df.set_index('Device Timestamp', inplace=True, drop=True)
-    print(type(df.index[0]))
+    # print(type(df.index[0]))
     df = df[df.index >= start_date]
     df = df[df.index <= end_date]
     df.reset_index(inplace=True)
@@ -93,7 +93,7 @@ def Set_Meds(avg_df, meds):
                 avg_df.loc[date, name] = 200
             else:
                 pass
-    print(avg_df)
+    # print(avg_df)
     # add 1's where appropriate
     return avg_df
 
@@ -111,16 +111,16 @@ meds = [cholestiramine, metformin, CoQ_10, ezetimibe]
 # get most recent data
 path = './most_recent_data/'
 files = os.listdir(path)
-print(files)
+# print(files)
 # create df
 df = pd.DataFrame()
 for file in files:
-    print(f'\nLoading file {file}...')
+    # print(f'\nLoading file {file}...')
     temp = pd.read_csv(path+file, header=1)
     df = df.append(temp)
 
 # prune df
-print('\nDropping unneeded columns...')
+# print('\nDropping unneeded columns...')
 df.drop(['Device', 'Serial Number',
         'Non-numeric Rapid-Acting Insulin', 'Rapid-Acting Insulin (units)',
          'Carbohydrates (grams)', 'Carbohydrates (servings)',
@@ -129,7 +129,7 @@ df.drop(['Device', 'Serial Number',
          'User Change Insulin (units)', 'Strip Glucose mg/dL'], inplace=True, axis=1)
 
 # convert timestamps to datetime
-print('\nConverting Timestamps...')
+# print('\nConverting Timestamps...')
 df['Device Timestamp'] = pd.to_datetime(df['Device Timestamp'], format="%m-%d-%Y %I:%M %p")
 
 default_start = df['Device Timestamp'].min()
@@ -138,7 +138,7 @@ default_end = df['Device Timestamp'].max()
 end_date = pd.to_datetime(st.date_input('End_Date', value=default_end))
 
 df = Limit_to_Current(df, start_date, end_date)
-print('Limited to Current...')
+# print('Limited to Current...')
 # create df.Glu from measures
 df = Combine_Glu(df)
 
@@ -151,18 +151,18 @@ df.drop_duplicates(inplace=True)
 
 # create ave_df for mean glucose
 avg_df = Create_Avg_DF(df)
-print(avg_df)
+# print(avg_df)
 
-print(df)
+# print(df)
 # create std_df for mean glucose
 std_df = Create_Std_DF(df)
-print(std_df)
+# print(std_df)
 
 
 # add meds to the df
 avg_df = Set_Meds(avg_df, meds)
 
-print('\nGenerating plot...')
+# print('\nGenerating plot...')
 fig, ax = plt.subplots(figsize=(15, 8))
 sns.set_style('dark')
 ax.plot(df.index, df['Glucose'], label='Glu', alpha=.4)
