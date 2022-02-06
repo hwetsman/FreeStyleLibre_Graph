d4041f88a7bac6a6df42a7bf29b619113134ffea
hwetsman <howard@tocdr.com>
2022-02-06 04:11:43 -0600

    Code:ode: create and execute feature engineering function

--- supervised_model.py
+++ supervised_model.py
@@ -25,6 +25,26 @@ matplotlib.use('TkAgg')
 pd.options.mode.chained_assignment = None
 
 
+def Feature_Eng(df):
+    """
+    This function takes the input df, drops unneeded columns, renames others,
+    normalizes Notes col by lowering capital letters, and combining the three
+    glucose measures into a single glucose column.
+    input: pandas df
+    output: pandas df
+    """
+    df.drop(['Device', 'Serial Number',
+            'Non-numeric Rapid-Acting Insulin', 'Rapid-Acting Insulin (units)',
+             'Carbohydrates (grams)', 'Carbohydrates (servings)',
+             'Non-numeric Long-Acting Insulin', 'Long-Acting Insulin (units)',
+             'Ketone mmol/L', 'Meal Insulin (units)', 'Correction Insulin (units)',
+             'User Change Insulin (units)', 'Strip Glucose mg/dL'], inplace=True, axis=1)
+    df.rename(columns={'Device Timestamp': 'DateTime'}, inplace=True)
+    df['Notes'] = df['Notes'].str.lower()
+    df = Combine_Glu(df)
+    return df
+
+
 # get most recent data
 path = './most_recent_data/'
 # files = os.listdir(path)
@@ -35,3 +55,6 @@ df = pd.read_csv(path+file, header=1)
 # convert timestamps to datetime
 print('\nConverting Timestamps...')
 df['Device Timestamp'] = pd.to_datetime(df['Device Timestamp'], format="%m-%d-%Y %I:%M %p")
+# Engineer Features
+print('\nDropping unneeded columns...')
+df = Feature_Eng(df)
