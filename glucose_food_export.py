176ea73ecb9493faddc38b4edeef9bc08551d4bb
hwetsman <howard@tocdr.com>
2022-08-01 16:21:36 -0500

    Refactor: create food by day df

--- glucose_food_export.py
+++ glucose_food_export.py
@@ -6,6 +6,7 @@ import numpy as np
 from datetime import datetime, timedelta
 import seaborn as sns
 import streamlit as st
+# from nltk.corpus import stopwords
 
 
 pd.set_option('display.max_columns', 500)
@@ -67,6 +68,85 @@ def Get_Glu_By_Day(glu_only):
     return glu_by_day
 
 
+def Combine_Notes(df1):
+    notes_only = df1[['Notes', 'Device Timestamp']]
+    notes_only = notes_only.rename(columns={'Device Timestamp': 'DateTime'})
+    notes_only.Notes = notes_only.Notes.fillna('')
+    notes_only.set_index('DateTime', inplace=True, drop=True)
+    st.write('before groupby', notes_only)
+    daily_notes = notes_only.groupby(pd.Grouper(freq='d'))[
+        'Notes'].apply(' '.join).reset_index(drop=False)
+    daily_notes['next'] = 0
+
+    daily_notes.Notes = daily_notes.Notes.str.replace('with', '')
+    daily_notes.Notes = daily_notes.Notes.str.replace('from', '')
+    daily_notes.Notes = daily_notes.Notes.str.replace('and', '')
+    daily_notes.Notes = daily_notes.Notes.str.replace('piece of', '')
+    daily_notes.Notes = daily_notes.Notes.str.replace(' w ', ' ')
+    daily_notes.Notes = daily_notes.Notes.str.replace(' x ', ' ')
+    daily_notes.Notes = daily_notes.Notes.str.replace(' mg ', ' ')
+    daily_notes.Notes = daily_notes.Notes.str.replace(' pb ', ' peanutbutter ')
+    daily_notes.Notes = daily_notes.Notes.str.replace(' peanut butter ', ' peanutbutter ')
+    daily_notes.Notes = daily_notes.Notes.str.replace('after', '')
+    daily_notes.Notes = daily_notes.Notes.str.replace('exercise', '')
+    daily_notes.Notes = daily_notes.Notes.str.replace('shower', '')
+    daily_notes.Notes = daily_notes.Notes.str.replace('meds', '')
+    daily_notes.Notes = daily_notes.Notes.str.replace(' on ', ' ')
+    daily_notes.Notes = daily_notes.Notes.str.replace('     ', ' ')
+    daily_notes.Notes = daily_notes.Notes.str.replace('    ', ' ')
+    daily_notes.Notes = daily_notes.Notes.str.replace('   ', ' ')
+    daily_notes.Notes = daily_notes.Notes.str.replace('  ', ' ')
+    daily_notes.Notes = daily_notes.Notes.str.replace(',', '')
+    daily_notes.Notes = daily_notes.Notes.str.replace('-', ' ')
+    daily_notes.Notes = daily_notes.Notes.str.replace('.', ' ')
+    daily_notes.Notes = daily_notes.Notes.str.replace('finger stick', '')
+    daily_notes.Notes = daily_notes.Notes.str.replace('blood stick', '')
+    daily_notes.Notes = daily_notes.Notes.str.replace('\d+', '')
+    daily_notes.Notes = daily_notes.Notes.str.lower()
+    string = set(' '.join(daily_notes.Notes).split(' '))
+    drop = ['', 'reveratrol', 'in', 'dao', 'a', 'l-methylfolate', 'cromalyn', 'glutathione', 'ago', 'flonase',
+            'little', 'fast', 'claritinzantac', 'ten', 'note', 'yesterday', 'chromium', 'took', 'cold', 'feeling', 'zantac',
+            'humans', 'finger', 'heat', 'hot', 'kind', 'started', 'tylenol', 'cholestiramine', 'strength', 'stick', 'that',
+            'bite', 'without', 'claritin', 'vsl', "mosca's", 'baby', 'after', 'lunch', 'one', 'exercise', 'this', 'black',
+            'all', 'zantaz', 'from', 'local', 'tow', 'x', ' ', 'scoop', 'vit', 'soft', 'string', 'glucose', 'big', 'chromium',
+            'two', 'got', 'enzymes', 'enlytezantac', 'an', 'on', 'no', 'nad', 'red', 'added', 'walk', 'as', 'asa', 'resveratrol',
+            'prednisone', 'artifact', 'four', 'hard', 'sonsor', 'enlyte', 'stevia', 'extra', 'with', 'it', '/', 'd', 'dinner',
+            'tums', 'late', 'same', 'binders', 'now', 'histamine', 'headache', 'at', 'k', 'w', 'mg', 'd', '/', 'of', 'hr',
+            'frig', 'frig', 'next']
+    int_string = [x for x in string if x not in drop]
+    string_3 = [x for x in int_string if x not in drop]
+    new_string = []
+    for word in int_string:
+        if ',' in word:
+            new_string.append(word.replace(',', ''))
+        elif 'x,' in word:
+            new_string.append(word.replace('x,', ''))
+        elif '.' in word:
+            new_string.append(word.replace('.', ''))
+        elif '/' in word:
+            new_string.append(word.replace('/', ''))
+        else:
+            new_string.append(word)
+    new_string = list(set(new_string))
+    for col in new_string:
+        daily_notes[col] = 0
+    a = st.empty()
+    b = st.empty()
+    a.write('I am working your data...')
+    for idx, r in daily_notes.iterrows():
+        starting_list = daily_notes.loc[idx, 'Notes'].split(' ')
+        if len(starting_list) == 1:
+            pass
+        else:
+            working_list = [x for x in starting_list if x not in drop]
+            for word in working_list:
+                b.write(idx)
+                daily_notes.loc[idx, word] = 1+daily_notes.loc[idx, word]
+    daily_notes.drop(['Notes', 'next'], axis=1, inplace=True)
+    daily_notes = daily_notes.rename(columns={'DateTime': 'date'})
+    return daily_notes
+
+
 st.set_page_config(layout="wide")
 
 path = './most_recent_data/'
@@ -92,24 +172,14 @@ df.drop(['Device', 'Serial Number',
 df1 = df.drop_duplicates()
 glu_only = Combine_Glu(df1)
 
-st.write('df1', df1)
+# st.write('df1', df1)
 
-
-def Combine_Notes(df1):
-    notes_only = df1[['Notes', 'Device Timestamp']]
-    notes_only = notes_only.rename(columns={'Device Timestamp': 'DateTime'})
-    # notes_only.set_index('DateTime', inplace=True, drop=True)
-    st.write('before groupby', notes_only)
-    notes_only.Notes.fillna('')
-    daily_notes = notes_only.groupby('DateTime')['Notes'].apply(' '.join)
-    # df = DATA.groupby('ID')['CODE'].apply(',' .join).reset_index(drop = False)
-    # daily_notes = notes_only.groupby('DateTime').agg(lambda x: ''.join(set(x)))
-    return daily_notes
-
-
-notes_only = Combine_Notes(df1)
-st.write('notes only', notes_only)
+food_by_day = Combine_Notes(df1)
+# st.write('notes only', food_by_day)
 
 
 glu_by_day = Get_Glu_By_Day(glu_only)
 # st.write(glu_by_day)
+
+freestyle_by_day = pd.merge(glu_by_day, food_by_day, on='date', how='outer')
+st.write(freestyle_by_day)
